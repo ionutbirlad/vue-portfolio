1f5dae2e056b7a0588e6c1ee08bc3c199653a1bb
Ionut Birlad <birladionut@yahoo.it>
2020-07-29 19:25:44 +0200

    installato standardjs e corretti relativi files

--- src/main.js
+++ src/main.js
@@ -15,15 +15,15 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import Argon from "./plugins/argon-kit";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import Argon from './plugins/argon-kit'
 import './registerServiceWorker'
 
-Vue.config.productionTip = false;
-Vue.use(Argon);
+Vue.config.productionTip = false
+Vue.use(Argon)
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,29 +1,29 @@
-import Vue from "vue";
-import Router from "vue-router";
-import AppHeader from "./layout/AppHeader";
-import AppFooter from "./layout/AppFooter";
-import Profile from "./views/Profile.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import AppHeader from './layout/AppHeader'
+import AppFooter from './layout/AppFooter'
+import Profile from './views/Profile.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  linkExactActiveClass: "active",
+  linkExactActiveClass: 'active',
   routes: [
     {
-      path: "/",
-      name: "profile",
+      path: '/',
+      name: 'profile',
       components: {
         header: AppHeader,
         default: Profile,
         footer: AppFooter
       }
-    },
+    }
   ],
   scrollBehavior: to => {
     if (to.hash) {
-      return { selector: to.hash };
+      return { selector: to.hash }
     } else {
-      return { x: 0, y: 0 };
+      return { x: 0, y: 0 }
     }
   }
-});
+})
